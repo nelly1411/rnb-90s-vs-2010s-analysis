79239bff1eb322339051e55b132d489b7d27a099
nelly1411 <nellyderulo@gmail.com>
2026-08-30 12:17:28 +0200

    Add Streamlit app

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="90s vs 10s RnB Dashboard", page_icon="🎶", layout="wide")
+
+st.title("90s vs 10s RnB 🎶")
+st.write("Wähle links eine Seite aus:")
+st.markdown("- **🏠 Dataset-Übersicht**: Überblick & Kennzahlen zum Datensatz")
+st.markdown("- **📊 Visualisierung**: Filter + Charts (90s vs 10s)")
